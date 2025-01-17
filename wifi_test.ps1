f134d2f0798041c1503ab56bd5f988d84f8fa2f9
Justin Garza <jgarza9788@gmail.com>
2025-01-17 12:07:35 -0500

    wifi-test 01/17/2025 12:07:31

--- wifi_test.ps1
+++ wifi_test.ps1
@@ -1,41 +1,41 @@
 
 $thisDate = Get-Date
 
-# test Error 404
-netsh wlan connect name="Error 404"
-Start-Sleep -Seconds 10.0
-.\pinger.ps1
-Start-Sleep -Seconds 5.0
-Write-Host ""
+# # test Error 404
+# netsh wlan connect name="Error 404"
+# Start-Sleep -Seconds 10.0
+# .\pinger.ps1
+# Start-Sleep -Seconds 5.0
+# Write-Host ""
 
-git add *
-git commit -m "wifi-test $($thisDate)"
-git push
-Write-Host ""
+# git add *
+# git commit -m "wifi-test $($thisDate)"
+# git push
+# Write-Host ""
 
-# test Error 404_EXT
-netsh wlan connect name="Error 404_EXT"
-Start-Sleep -Seconds 10.0
-.\pinger.ps1
-Start-Sleep -Seconds 5.0
-Write-Host ""
+# # test Error 404_EXT
+# netsh wlan connect name="Error 404_EXT"
+# Start-Sleep -Seconds 10.0
+# .\pinger.ps1
+# Start-Sleep -Seconds 5.0
+# Write-Host ""
 
-git add *
-git commit -m "wifi-test $($thisDate)"
-git push
-Write-Host ""
+# git add *
+# git commit -m "wifi-test $($thisDate)"
+# git push
+# Write-Host ""
 
-# test Error 404 NH
-netsh wlan connect name="Error 404 NH"
-Start-Sleep -Seconds 10.0
-.\pinger.ps1
-Start-Sleep -Seconds 5.0
-Write-Host ""
+# # test Error 404 NH
+# netsh wlan connect name="Error 404 NH"
+# Start-Sleep -Seconds 10.0
+# .\pinger.ps1
+# Start-Sleep -Seconds 5.0
+# Write-Host ""
 
-git add *
-git commit -m "wifi-test $($thisDate)"
-git push
-Write-Host ""
+# git add *
+# git commit -m "wifi-test $($thisDate)"
+# git push
+# Write-Host ""
 
 # generate report
 jupyter nbconvert --to notebook --execute reports.ipynb
