c3f52dfd30ac37ba67af3310f59aba21f1b052eb
Justin Garza <jgarza9788@gmail.com>
2025-03-15 23:29:57 -0400

    wifi-test 03/15/2025 23:27:44

--- wifi_test.ps1
+++ wifi_test.ps1
@@ -1,43 +1,66 @@
 
 $thisDate = Get-Date
 
+
+function Connect-ToWiFi {
+    param (
+        [string]$wifiName,
+        [int]$retryInterval = 10,
+        [int]$maxTries = 5
+    )
+
+    $attempts = 0
+
+    while ($attempts -lt $maxTries) {
+        Write-Host "Connecting to $wifiName..."
+        
+        $output = netsh wlan connect name=$wifiName
+        if ($output -notmatch "is not available to connect") {
+            Write-Host "✅ $wifiName" -ForegroundColor Green
+            return $true
+        }
+
+        $attempts++
+        Write-Host "🔁 Retry $retryInterval seconds..." -ForegroundColor Yellow
+        Start-Sleep -Seconds $retryInterval
+    }
+
+    Write-Host "❌ $wifiName" -ForegroundColor Red
+    return $false
+}
+
+
+function DoGit()
+{
+    git add *
+    git commit -m "wifi-test $($thisDate)"
+    git push
+    Write-Host "✅ pushed"
+}
+
+
 # test Error 404
-netsh wlan connect name="Error 404"
-Start-Sleep -Seconds 20.0
+Connect-ToWiFi -wifiName "Error 404"
+Start-Sleep -Seconds 10.0
 .\pinger.ps1
 Write-Host ""
-
-git add *
-git commit -m "wifi-test $($thisDate)"
-git push
-Write-Host ""
+DoGit
 
 # test Error 404_EXT
-netsh wlan connect name="Error 404_EXT"
-Start-Sleep -Seconds 20.0
+Connect-ToWiFi -wifiName "Error 404_EXT"
+Start-Sleep -Seconds 10.0
 .\pinger.ps1
 Write-Host ""
-
-git add *
-git commit -m "wifi-test $($thisDate)"
-git push
-Write-Host ""
+DoGit
 
 # test Error 404 NH
-netsh wlan connect name="Error 404 NH"
-Start-Sleep -Seconds 20.0
+Connect-ToWiFi -wifiName "Error 404 NH"
+Start-Sleep -Seconds 10.0
 .\pinger.ps1
 Write-Host ""
+DoGit
 
-git add *
-git commit -m "wifi-test $($thisDate)"
-git push
-Write-Host ""
 
 # generate report
 jupyter nbconvert --to python --execute reports.ipynb
-
-git add *
-git commit -m "wifi-test $($thisDate)"
-git push
-Write-Host ""
+DoGit
